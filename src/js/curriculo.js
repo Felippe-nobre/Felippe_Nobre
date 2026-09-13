39e098fa2d76544bbe11eda34b8f26fb82c20d1b
Felippe <113641116+Felippe-nobre@users.noreply.github.com>
2026-09-13 02:02:36 -0300

    Remove currículo e script do versionamento

--- src/js/curriculo.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('btn-download').addEventListener('click', function () {
-  document.getElementById('btn-download').style.display = 'none';
-  window.print();
-  setTimeout(() => {
-    document.getElementById('btn-download').style.display = 'block';
-  }, 500);
-});
